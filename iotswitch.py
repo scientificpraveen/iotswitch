056520dc9661fbca0db9cc44f191f28861ba06e5
Praveen Kumar <33199932+scientificpraveen@users.noreply.github.com>
2024-11-24 16:14:48 +0530

    Update iotswitch.py

--- iotswitch.py
+++ iotswitch.py
@@ -28,4 +28,4 @@ def handle_pins():
             return jsonify({"error": "Invalid pin"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=2000)
